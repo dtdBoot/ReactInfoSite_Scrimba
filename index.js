b971556b9780699109c764ea50e069227d84f9ab
CharlesHLam <CharlesHLam@users.noreply.github.com>
2024-04-01 14:30:45 -0400

    fixed index.js with basic React render statement

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ the line of code you just learned! Nope, not on my watch!
 
 Try to write that 1-liner of React code again! This time,
 see if you can figure out how to render an <ul> with 2+ <li>s inside*/
-ReactDOM.render(
-    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
-    document.getElementById("root")
+
+const navbar = (
+    <nav className="navbar">
+        <h1 className="heading1">
+            Great Websites R Us
+        </h1>
+        <ul>
+            <li>Pricing</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
+
+ReactDOM.render(navbar, document.getElementById('root'));
